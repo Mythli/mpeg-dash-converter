449d64e135007a7f0215fd0972bf37036a8c1ee2
Tobias <github@mythli.net>
2024-03-11 18:02:57 +0300

    x265 fix

--- src/dashify.sh
+++ src/dashify.sh
@@ -8,8 +8,13 @@ default_max_crf=28
 default_steps=4
 default_preset="slower"
 default_x265=0 # x265 encoding disabled by default
+default_x264=0 # x265 encoding disabled by default
+default_vp8=0 # x265 encoding disabled by default
+default_vp9=0 # x265 encoding disabled by default
 available_steps=(1 1.5 3 6 9 12 12 12)
 available_audio_steps=(1 2 2 2 2 2 2 2)
+
+# If the flag is turned on a folder should be created for the codec and a separate dash should be created for each codec
 
 # Usage information
 usage() {
@@ -20,8 +25,11 @@ usage() {
   echo "  --min-crf <value>        Set the minimum CRF value for the highest quality level (default: $default_min_crf)."
   echo "  --max-crf <value>        Set the maximum CRF value for the lowest quality level (default: $default_max_crf)."
   echo "  --steps <number>         Set the number of resolution steps for the output videos (default: $default_steps)."
-  echo "  --preset <preset>        Set the x264 encoding preset (default: $default_preset)."
-  echo "                     Enable x265 encoding for HEVC compatibility."
+  echo "  --preset <preset>        Set the x264 encoding preset (default: $default_preset). This option is also mapped to vp8 and vp9 encoding speed"
+  #echo "  --vp8                    Enable vp8 encoding"
+  #echo "  --vp9                    Enable vp9 encoding"
+  echo "  --x264                   Enable x264 encoding"
+  echo "  --x265                   Enable x265 encoding for HEVC compatibility."
   echo
   echo "  <input_files...>         One or more input video files to be processed."
   echo
@@ -36,6 +44,9 @@ max_crf=$default_max_crf
 steps=$default_steps
 preset=$default_preset
 x265=$default_x265
+vp8=$default_vp8
+vp9=$default_vp9
+x264=$default_x264
 files=()
 
 while [[ $# -gt 0 ]]; do
@@ -60,6 +71,18 @@ while [[ $# -gt 0 ]]; do
       preset="$2"
       shift 2
       ;;
+    --vp8)
+      vp8=1
+      shift
+      ;;
+    --vp9)
+      vp9=1
+      shift
+      ;;
+    --x264)
+      x264=1
+      shift
+      ;;
     --x265)
       x265=1
       shift
@@ -70,6 +93,11 @@ while [[ $# -gt 0 ]]; do
       ;;
   esac
 done
+
+# if no codec is selected, default to x264
+if [ $x264 -eq 0 ] && [ $x265 -eq 0 ] && [ $vp8 -eq 0 ] && [ $vp9 -eq 0 ]; then
+  x264=1
+fi
 
 # If no input files are provided, display usage information
 if [ ${#files[@]} -eq 0 ]; then
@@ -99,6 +127,143 @@ gcd() {
   echo $a
 }
 
+# Function to map x264 preset to vp8/vp9 speed
+calculate_speed() {
+  local preset=$1
+  case "$preset" in
+    ultrafast)
+      echo 4
+      ;;
+    superfast)
+      echo 3
+      ;;
+    veryfast)
+      echo 2
+      ;;
+    faster)
+      echo 1
+      ;;
+    fast)
+      echo 1
+      ;;
+    medium)
+      echo 0
+      ;;
+    slow)
+      echo 0
+      ;;
+    slower)
+      echo 0
+      ;;
+    veryslow)
+      echo 0
+      ;;
+    *)
+      echo "Unsupported preset: $preset"
+      exit 1
+      ;;
+  esac
+}
+
+speed=$(calculate_speed "$preset")
+
+convert_with_vp8() {
+  local input_file=$1
+  local output_dir=$2
+  local base=$3
+  local scaled_width=$4
+  local scaled_height=$5
+  # First pass
+  ffmpeg -y -i "$input_file" -c:v libvpx -b:v 0 -crf "$crf" -speed 4 -pass 1 -an -vf "scale=${scaled_width}:${scaled_height}" -f webm /dev/null
+  # Second pass
+  ffmpeg -i "$input_file" -c:v libvpx -b:v 0 -crf "$crf" -speed "$speed" -pass 2 -an -vf "scale=${scaled_width}:${scaled_height}" -f webm "${output_dir}/${base}_${scaled_width}x${scaled_height}_vp8_crf${crf}.webm"
+}
+
+convert_with_vp9() {
+  local input_file=$1
+  local output_dir=$2
+  local base=$3
+  local scaled_width=$4
+  local scaled_height=$5
+  local crf=$6
+
+  # First pass
+  ffmpeg -y -i "$input_file" -c:v libvpx-vp9 -b:v 0 -crf "$crf" -speed 4 -pass 1 -an -vf "scale=${scaled_width}:${scaled_height}" -f webm /dev/null
+  # Second pass
+  ffmpeg -i "$input_file" -c:v libvpx-vp9 -b:v 0 -crf "$crf" -speed "$speed" -pass 2 -an -vf "scale=${scaled_width}:${scaled_height}" -f webm "${output_dir}/${base}_${scaled_width}x${scaled_height}_vp9_crf${crf}.webm"
+}
+
+convert_with_libx264() {
+  local input_file=$1
+  local output_dir=$2
+  local base=$3
+  local scaled_width=$4
+  local scaled_height=$5
+  local crf=$6
+  ffmpeg -y -i "$input_file" -an -c:v libx264 -preset "$preset" -x264opts keyint="$framerate":min-keyint="$framerate":no-scenecut -vf "scale=${scaled_width}:${scaled_height}" -crf "$crf" -f mp4 "${output_dir}/${base}_${scaled_width}x${scaled_height}_x264_crf${crf}.mp4"
+}
+
+convert_with_libx265() {
+  local input_file=$1
+  local output_dir=$2
+  local base=$3
+  local scaled_width=$4
+  local scaled_height=$5
+  local crf=$6
+
+   ffmpeg -y -i "$input_file" -an -c:v libx265 -preset "$preset" \
+      -x265-params "keyint=${framerate}:min-keyint=${framerate}:no-scenecut=1" \
+      -vf "scale=${scaled_width}:${scaled_height}" -crf "$crf" \
+      -f mp4 "${output_dir}/${base}_${scaled_width}x${scaled_height}_x265_crf${crf}.mp4"
+}
+
+# Function to convert audio with open-source codec
+convert_audio_opensource() {
+  local input_file=$1
+  local output_audio=$2
+  local audio_bitrate=$3
+  ffmpeg -i "$input_file" -vn -c:a libvorbis -b:a "${audio_bitrate}k" "$output_audio"
+}
+
+# Function to convert audio with proprietary codec
+convert_audio_proprietary() {
+  local input_file=$1
+  local output_audio=$2
+  local audio_bitrate=$3
+  ffmpeg -i "$input_file" -vn -c:a aac -b:a "${audio_bitrate}k" "$output_audio"
+}
+
+# Function to convert audio based on video codec
+convert_audio() {
+  local input_file=$1
+  local output_dir=$2
+  local base=$3
+  local video_codec=$4
+  local original_bitrate=128
+
+  # Determine if an open-source video codec is used like vp8 or vp9, use the open source function, else use the proprietary function
+  if [[ "$video_codec" == "vp8" || "$video_codec" == "vp9" ]]; then
+    for ((i=0; i<steps; i++)); do
+      local audio_bitrate=$(bc <<< "scale=0; $original_bitrate / ${available_audio_steps[$i]} / 1")
+      if [ -f "${output_dir}/${base}_audio_${audio_bitrate}.ogg" ]; then
+        echo "File exists"
+      else
+        convert_audio_opensource "$input_file" "${output_dir}/${base}_audio_${audio_bitrate}.ogg" "$audio_bitrate"
+      fi
+    done
+  else
+    for ((i=0; i<steps; i++)); do
+      local audio_bitrate=$(bc <<< "scale=0; $original_bitrate / ${available_audio_steps[$i]} / 1")
+
+      if [ -f "${output_dir}/${base}_audio_${audio_bitrate}.m4a" ]; then
+        echo "File exists"
+      else
+        convert_audio_proprietary "$input_file" "${output_dir}/${base}_audio_${audio_bitrate}.m4a" "$audio_bitrate"
+      fi
+    done
+  fi
+}
+
 # Function to convert video
 convert_video() {
   local input_file=$1
@@ -106,14 +271,15 @@ convert_video() {
   local base=$3
   local original_width=$4
   local original_height=$5
-  local codec=$6 # Add codec as a parameter
+  local codec=$6
+  #local steps=$7
+  #local min_crf=$8
+  #local max_crf=$9
+  #local preset=${10}
+  #local framerate=${11}
+  #local available_steps=("${@:12}") # Array of available steps for video scaling
 
-  # Audio conversion at various bitrates
-  local original_bitrate=128 # Original audio bitrate in kbps
-  for ((i=0; i<steps; i++)); do
-    local audio_bitrate=$(bc <<< "scale=0; $original_bitrate / ${available_audio_steps[$i]} / 1")
-    ffmpeg -y -i "$input_file" -c:a aac -ac 2 -b:a "${audio_bitrate}k" -vn "${output_dir}/${base}_audio_${audio_bitrate}.m4a"
-  done
+  convert_audio "$input_file" "$output_dir" "$base" "$codec"
 
   # Calculate GCD and aspect ratio
   local gcd=$(gcd $original_width $original_height)
@@ -121,7 +287,9 @@ convert_video() {
   local aspect_ratio_height=$((original_height / gcd))
 
   # Calculate CRF increment for each step
-  local crf_increment=$(bc <<< "scale=2; ($max_crf - $min_crf) / ($steps - 1)")
+
+  local steps_adjusted=$((steps > 1 ? steps : 2))
+  local crf_increment=$(bc <<< "scale=2; ($max_crf - $min_crf) / ($steps_adjusted - 1)")
 
   # Video conversion at various resolutions and CRF values
   for ((i=0; i<steps; i++)); do
@@ -140,46 +308,34 @@ convert_video() {
       scaled_width=$original_width
     fi
 
-    # Determine the codec and file extension
-    local codec_name
-    local file_extension
-    local codec_options
-    if [ "$codec" == "libx264" ]; then
-      codec_name="x264"
-      file_extension="mp4"
-      codec_options="-preset $preset -x264opts keyint=${framerate}:min-keyint=${framerate}:no-scenecut"
-    elif [ "$codec" == "libx265" ]; then
-      codec_name="x265"
-      file_extension="mp4"
-      codec_options="-preset $preset -x265-params keyint=${framerate}:min-keyint=${framerate}:no-scenecut=1"
+    # Call the appropriate codec-specific conversion function
+    if [ "$codec" == "x264" ]; then
+      convert_with_libx264 "$input_file" "$output_dir" "$base" "$scaled_width" "$scaled_height" "$crf"
+    elif [ "$codec" == "x265" ]; then
+      convert_with_libx265 "$input_file" "$output_dir" "$base" "$scaled_width" "$scaled_height" "$crf"
+    elif [ "$codec" == "vp8" ]; then
+      convert_with_vp8 "$input_file" "$output_dir" "$base" "$scaled_width" "$scaled_height" "$crf"
+    elif [ "$codec" == "vp9" ]; then
+      convert_with_vp9 "$input_file" "$output_dir" "$base" "$scaled_width" "$scaled_height" "$crf"
     else
       echo "Unsupported codec: $codec"
       exit 1
     fi
-
-    ffmpeg -y -i "$input_file" -an -c:v "$codec" $codec_options -vf "scale=${scaled_width}:${scaled_height}" -crf "$crf" -f mp4 "${output_dir}/${base}_${scaled_width}x${scaled_height}_${codec_name}_crf${crf}.${file_extension}"
   done
 }
 
-# Function to generate DASH files
 generate_dash() {
-  local video_dir=$1
-  local audio_dir=$2
-  local output_dir=$3
+  local media_dir=$1
+  local output_dir=$2
 
-  # Find all x265 and x264 files, sort x265 files first
-  local video_files=($(find "$video_dir" -name "*_x265_*.mp4" -exec echo {} \; | sort) $(find "$video_dir" -name "*_x264_*.mp4" -exec echo {} \; | sort))
-
-  # Find all audio files
-  local audio_files=($(find "$audio_dir" -name "*_audio_*.m4a" -exec echo {} \; | sort))
-
-  # Create the MP4Box command with sorted video files
+  # Create the MP4Box command with all media files in the directory
   local mp4box_cmd="MP4Box -dash 12000 -rap -frag-rap -profile live -bs-switching no -out \"${output_dir}/stream.mpd\""
-  for video_file in "${video_files[@]}"; do
-    mp4box_cmd+=" \"$video_file\""
-  done
-  for audio_file in "${audio_files[@]}"; do
-    mp4box_cmd+=" \"$audio_file\""
+
+  # Add all video and audio files to the MP4Box command
+  for media_file in "${media_dir}"/*.{mp4,webm,m4a,ogg}; do
+    if [ -f "$media_file" ]; then
+      mp4box_cmd+=" \"$media_file\""
+    fi
   done
 
   # Execute the MP4Box command
@@ -188,35 +344,39 @@ generate_dash() {
 
 # Main loop to process files
 for input_file in "${files[@]}"; do
+
   filename=$(basename -- "$input_file")
   dirname=$(dirname -- "$input_file")
   base="${filename%.*}"
-  dash_dir="${dirname}/${base}.dash"
-  tmp_dir="${dirname}/${base}.tmp"
-
-  # Delete the tmp and dash directories if they already exist
-  rm -rf "$dash_dir"
-  rm -rf "$tmp_dir"
-
-  # Create new tmp and dash directories
-  mkdir -p "$dash_dir"
-  mkdir -p "$tmp_dir"
 
   echo "Converting \"$filename\" to multi-bitrate video in MPEG-DASH"
 
   original_width=$(ffprobe -v error -select_streams v:0 -show_entries stream=width -of default=noprint_wrappers=1:nokey=1 "$input_file")
   original_height=$(ffprobe -v error -select_streams v:0 -show_entries stream=height -of default=noprint_wrappers=1:nokey=1 "$input_file")
 
-  # Convert using x264 codec
-  convert_video "$input_file" "$tmp_dir" "$base" "$original_width" "$original_height" "libx264"
+  # Process each codec separately
+  for codec in "x264" "x265" "vp8" "vp9"; do
+    if [ "${!codec}" -eq 1 ]; then
+      codec_dir="${dirname}/${base}_${codec}"
+      tmp_dir="${codec_dir}.tmp"
+      dash_dir="${codec_dir}.dash"
 
-  # If x265 option is set, convert using x265 codec
-  if [ "$x265" -eq 1 ]; then
-    convert_video "$input_file" "$tmp_dir" "$base" "$original_width" "$original_height" "libx265"
-  fi
+      # Delete the tmp and dash directories if they already exist
+      rm -rf "$dash_dir"
+      rm -rf "$tmp_dir"
 
-  generate_dash "$tmp_dir" "$tmp_dir" "$dash_dir"
+      # Create new tmp and dash directories
+      mkdir -p "$dash_dir"
+      mkdir -p "$tmp_dir"
 
-  # Remove temporary files
-  #rm -rf "$tmp_dir"
+      # Convert video using the specified codec
+      convert_video "$input_file" "$tmp_dir" "$base" "$original_width" "$original_height" "${codec}"
+
+      # Generate DASH files for the codec
+      generate_dash "$tmp_dir" "$dash_dir"
+
+      # Remove temporary files
+      #rm -rf "$tmp_dir"
+    fi
+  done
 done
